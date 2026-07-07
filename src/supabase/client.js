4d14fc769080f43c61947736b5e518492a08f57b
Sourabh Sahu <Sourabh.Sahu@flipsidegroup.com>
2026-07-07 11:52:15 +0530

    updated client supbase

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -9,19 +9,19 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-export const SUPABASE_URL = ''; // e.g. 'https://xxxx.supabase.co'
-export const SUPABASE_ANON_KEY = ''; // e.g. 'eyJ...'
+export const SUPABASE_URL = 'https://lgvizhpaczvvctivdjie.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxndml6aHBhY3p2dmN0aXZkamllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MDA3NDgsImV4cCI6MjA5ODk3Njc0OH0.WYkt0kxrpLZN3DlqKNg2UNw1tLDvMvRNyH0Xys1LVuc'
 
 export const isSupabaseConfigured = Boolean(SUPABASE_URL && SUPABASE_ANON_KEY);
 
 export const supabase = isSupabaseConfigured
   ? createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-      auth: {
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        // React Native has no URL to parse for OAuth redirects.
-        detectSessionInUrl: false,
-      },
-    })
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      // React Native has no URL to parse for OAuth redirects.
+      detectSessionInUrl: false,
+    },
+  })
   : null;
